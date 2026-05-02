a88a805373801d21b333b0fff7124044a70356ca
Bryan Galdámez <josuegalre@gmail.com>
2026-05-01 22:58:25 -0600

    chore(build): migrate advancedChunks to rollup manualChunks
    Replace deprecated rolldown advancedChunks with standard Rollup
    manualChunks function in rollupOptions.output. Eliminates build
    warning:
    'advancedChunks option is deprecated, please use codeSplitting instead.'
    
    Chunks preserved:
    - react: react, react-dom, scheduler
    - tanstack: @tanstack/*
    - i18n: i18next, react-i18next
    - vendor: all other node_modules
    
    Build passes cleanly with zero warnings.

--- apps/web-next/vite.config.ts
+++ apps/web-next/vite.config.ts
@@ -16,27 +16,21 @@ export default defineConfig({
     tsconfigPaths: true,
   },
   build: {
-    rolldownOptions: {
+    rollupOptions: {
       output: {
-        advancedChunks: {
-          groups: [
-            {
-              name: 'react',
-              test: /node_modules\/(react|react-dom|scheduler)\//,
-            },
-            {
-              name: 'tanstack',
-              test: /node_modules\/@tanstack\//,
-            },
-            {
-              name: 'i18n',
-              test: /node_modules\/(i18next|react-i18next)\//,
-            },
-            {
-              name: 'vendor',
-              test: /node_modules\//,
-            },
-          ],
+        manualChunks(id) {
+          if (/node_modules\/(react|react-dom|scheduler)\//.test(id)) {
+            return 'react'
+          }
+          if (/node_modules\/\@tanstack\//.test(id)) {
+            return 'tanstack'
+          }
+          if (/node_modules\/(i18next|react-i18next)\//.test(id)) {
+            return 'i18n'
+          }
+          if (/node_modules\//.test(id)) {
+            return 'vendor'
+          }
         },
       },
     },
