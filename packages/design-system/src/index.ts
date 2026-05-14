6cf636b243ae628a10241668f176f35732dc2f11
Bryan Galdámez <josuegalre@gmail.com>
2026-05-13 20:19:22 -0600

    chore(design-system): scaffold @shoma/design-system workspace package

---
+++ packages/design-system/src/index.ts
@@ -0,0 +1,60 @@
+export {
+  semanticTokenContract,
+  semanticTokenNames,
+} from "./tokens";
+export {
+  typographyFontFamilyNames,
+  typographyFontWeightNames,
+  typographyScaleNames,
+} from "./styles/typography";
+export type {
+  SemanticTokenContract,
+  SemanticTokenCssVariable,
+  SemanticTokenName,
+} from "./tokens";
+export type {
+  TypographyFontFamilyCssVariable,
+  TypographyFontFamilyName,
+  TypographyFontWeightCssVariable,
+  TypographyFontWeightName,
+  TypographyScaleCssVariable,
+  TypographyScaleName,
+} from "./styles/typography";
+
+export { Icon } from "./components/icon";
+export type { IconProps } from "./components/icon";
+export { Alert, AlertDescription, AlertTitle, alertVariants } from "./components/alert";
+export { Avatar } from "./components/avatar";
+export type { AvatarProps } from "./components/avatar";
+export { Badge, badgeVariants } from "./components/badge";
+export type { BadgeProps } from "./components/badge";
+export { BottomNav } from "./components/bottom-nav";
+export type { BottomNavProps } from "./components/bottom-nav";
+export { BottomSheet } from "./components/bottom-sheet";
+export { Button, buttonVariants } from "./components/button";
+export type { ButtonProps } from "./components/button";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/card";
+export {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./components/dropdown-menu";
+export { Input } from "./components/input";
+export type { InputProps } from "./components/input";
+export { Skeleton, SkeletonShimmer } from "./components/skeleton";
+export { Spinner } from "./components/spinner";
+export type { SpinnerProps } from "./components/spinner";
+
+export const designSystemThemeCss = "@shoma/design-system/src/styles/theme.css";
+export const designSystemTokensCss = "@shoma/design-system/src/styles/tokens.css";
