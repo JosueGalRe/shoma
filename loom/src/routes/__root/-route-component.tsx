0971d7316553c07210b16dabaab7f3b26af23a3f
Bryan Galdámez <josuegalre@gmail.com>
2026-05-29 22:07:46 -0600

    feat(routes): wire up PWA update prompt in root layout

--- loom/src/routes/__root/-route-component.tsx
+++ loom/src/routes/__root/-route-component.tsx
@@ -1,16 +1,22 @@
 import { Outlet } from '@tanstack/react-router'
 
 import { AppShell } from '@/components/layout'
+import { UpdatePrompt } from '@/components/update-prompt'
 import { useLcuCacheClear } from '@/core/lcu/use-lcu-cache-clear'
+import { usePwaUpdate } from '@/core/pwa/use-pwa-update'
 import { useGlobalSessionReconnect } from '@/lib/reconnect-utils'
 
 export function RootRouteComponent() {
   useGlobalSessionReconnect()
   useLcuCacheClear()
 
+  const { needsRefresh, update, dismiss } = usePwaUpdate()
+
   return (
     <AppShell>
       <Outlet />
+
+      {needsRefresh ? <UpdatePrompt onDismiss={dismiss} onUpdate={update} /> : null}
     </AppShell>
   )
 }
